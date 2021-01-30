d20ee48ce3f5fae907db1070aabbc26101c4ebd9
Karinecasant <77842351+Karinecasant@users.noreply.github.com>
2021-01-29 23:09:10 -0300

    Adicionando arquivos exercícios via upload-29/01/2021

---
+++ DesenvolvimentoJava/Aula4/src/Aula4/Exercicio2_Array.java
@@ -0,0 +1,10 @@
+// 2.	Um dado é lançado 10 vezes e o valor correspondente é anotado. 
+// Faça um programa que gere um vetor com os lançamentos, escreva esse vetor. 
+// A seguir determine e imprima a média aritmética dos lançamentos, contabilize e
+// apresente também quantas foram as ocorrências da maior pontuação
+
+package Aula4;
+
+public class Exercicio2_Array {
+
+}
